3b894c7a0c14e280c2514e471ab0369462406b1a
Gaurav Sahni <kinglygaurav@gmail.com>
2020-06-20 15:16:06 -0500

    Lesson-1

---
+++ NsbBasicsDocker/ClientUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace ClientUI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Client UI";
+            
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+
+            await endpointInstance.Stop().ConfigureAwait(false);
+        }
+    }
+}
