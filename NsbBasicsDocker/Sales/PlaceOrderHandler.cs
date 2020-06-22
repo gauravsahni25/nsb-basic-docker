434d4163c2aee084eb7da9efa8d3acbaa749eddb
Gaurav Sahni <kinglygaurav@gmail.com>
2020-06-21 20:31:33 -0500

    Retrying Errors

--- NsbBasicsDocker/Sales/PlaceOrderHandler.cs
+++ NsbBasicsDocker/Sales/PlaceOrderHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Messages.Commands;
 using Messages.Events;
 using NServiceBus;
@@ -8,11 +9,21 @@ namespace Sales
 {
     public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
     {
+        static Random random = new Random();
         static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
 
         public Task Handle(PlaceOrder message, IMessageHandlerContext context)
         {
             log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            // To see Poison message demo
+            // throw new Exception("BOOM");
+            
+            // To see transient error demo
+            //if (random.Next(0, 5) == 0)
+            //{
+            //    throw new Exception("Oops");
+            //}
 
             var orderPlaced = new OrderPlaced
             {
